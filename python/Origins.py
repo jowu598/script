bffd04292a57204650bc28e4c77a7aa30f443ff5
yangjun <jowu598@gmail.com>
2016-03-14 21:38:40 +0800

    update script

--- python/Origins.py
+++ python/Origins.py
@@ -1,17 +1,108 @@
 __author__ = 'jowu'
 
+# 0-3 versionId
+# 0-7 typeId
+# 0-1023 objId
+# 0-15 actionId
 import sys, getopt, os,  os.path,  re,  codecs
 from xml.dom import minidom
 from xml.dom.minidom import parse
 import xml.dom.minidom
 from biplist import *
-colours = ["red","green","blue"]
-s_enmyeName =
-    ["1",
-     "2",
-     "3"]
 
+s_enmyeName = \
+[
+    'arachnomancer_miniSpider', \
+    'arachnomancer_spider', \
+    'arachnomancer', \
+    'bandersnatch', \
+    'bloodServant', \
+    'bloodsydianGnoll', \
+    'bloodsydianWarlock', \
+    'zealot', \
+    'dark_spitters', \
+    'drider', \
+    'ettin', \
+    'fungusRider_medium', \
+    'fungusRider_small', \
+    'fungusRider', \
+    'gloomy', \
+    'gnoll_blighter', \
+    'gnoll_burner', \
+    'gnoll_gnawer', \
+    'gnoll_reaver', \
+    'gollem', \
+    'grim_devourers', \
+    'harraser', \
+    'hyena', \
+    'knocker', \
+    'mantaray', \
+    'gnollBerzerker', \
+    'mountedAvenger', \
+    'ogre_mage', \
+    'perython', \
+    'rabbit', \
+    'razorboar', \
+    'redcap', \
+    'satyrHoplite', \
+    'satyr', \
+    'scourger', \
+    'scourger_shadow', \
+    'screecher_bat', \
+    'shadow_champion', \
+    'shadow_spawn', \
+    'son_of_mactans', \
+    'sword_spider', \
+    'tarantula', \
+    'theBeheader', \
+    'twilight_avenger', \
+    'twilight_bannerbearer', \
+    'twilight_evoker', \
+    'twilight_heretic', \
+    'webspitterSpider', \
+]
 
+s_towerName = \
+[
+    'archer', \
+    'artillery_henge', \
+    'artillery_thrower', \
+    'artillery_tree', \
+    'mage', \
+    'forestKeeper', \
+    'bladeSinger' \
+]
+
+s_allyName = \
+[
+    'catapult', \
+    'ewok', \
+    'reinforce' \
+    ''
+]
+
+s_heroName = \
+[
+    'archer_hero', \
+    'bravebark', \
+    'alleria', \
+    'arivan', \
+    'bolverk', \
+    'bruce', \
+    'denas', \
+    'durax', \
+    'regson', \
+    'fallen_angel', \
+    'faustus', \
+    'lynn', \
+    'xin', \
+    'phoenix', \
+    'catha', \
+    'razzAndRaggs', \
+    'veznan', \
+    'wilburg', \
+    'malik' \
+]
 
 class  XmlFilter:
     def __init__(self):
@@ -44,7 +135,6 @@ class  XmlFilter:
         for name in self.enemyNames:
             print name
 
-
 class PListReader:
     def __init__(self):
         print('--PListFilter--')
@@ -62,6 +152,7 @@ class PListReader:
             for action in actions:
                 print (action)
                 #print ("formIndex %d toIndex %d prefix %s" %(action['fromIndex'], action['toIndex'], action['prefix']))
+
 
 
 
